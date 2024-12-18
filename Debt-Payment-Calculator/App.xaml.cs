b7962002fe74fcfb1ccf7101b351a0dbe9550182
DeionSloc <95542183+DeionSloc@users.noreply.github.com>
2024-12-17 22:06:28 -0700

    Initial commit
    Created .NET Maui project
    Created and initialized empty repository to house project

---
+++ Debt-Payment-Calculator/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Debt_Payment_Calculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ Debt-Payment-Calculator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Debt_Payment_Calculator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
